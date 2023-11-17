74d46023f7ab4ad14646be45dd9cd86c171e68f3
Matthias Hacksteiner <contact@matthiashacksteiner.net>
2023-11-17 09:29:15 +0100

    Update dependencies and fix minor issues in pages

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,27 +1,13 @@
 const API_URL = 'https://cms.baukasten.matthiashacksteiner.net';
 
-class KirbyApiError extends Error {
-	url;
-	status;
-
-	constructor(message, status, url) {
-		super(message);
-		this.status = status;
-		this.url = url;
-		this.message = message;
-		this.name = 'KirbyApiError';
-	}
-}
-
 // Reusable function for making GET requests
 async function fetchData(uri) {
 	const response = await fetch(API_URL + uri, {
 		method: 'GET',
 	});
-	// console.log('Fetching', uri, response.status, response.statusText);
+	console.log('Fetching', uri, response.status, response.statusText);
 	if (response.status !== 200) {
 		console.log('Error fetching', uri, response.status, response.statusText);
-		// throw new KirbyApiError(await response.text(), response.status, uri);
 	}
 	return response.json();
 }
